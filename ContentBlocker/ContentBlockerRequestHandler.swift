0b09aecaa93055cf139285c957c1bb3ff5754b2d
Jose Pimentel <josephb401@hotmail.com>
2025-07-12 23:27:02 -0400

    updated the blocker list

--- ContentBlocker/ContentBlockerRequestHandler.swift
+++ ContentBlocker/ContentBlockerRequestHandler.swift
@@ -5,18 +5,50 @@
 //  Created by Jose Pimentel on 7/9/25.
 //
 
+//import UIKit
+//import MobileCoreServices
+//
+//class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+//
+//    func beginRequest(with context: NSExtensionContext) {
+//        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+//        
+//        let item = NSExtensionItem()
+//        item.attachments = [attachment]
+//        
+//        context.completeRequest(returningItems: [item], completionHandler: nil)
+//    }
+//    
+//}
+
+//import SafariServices
+//
+//class ContentBlockerRequestHandler: SFContentBlockerManager {
+//    override func beginRequest(with context: NSExtensionContext) {
+//        if let attachment = try? NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json")!) {
+//            let item = NSExtensionItem()
+//            item.attachments = [attachment]
+//            context.completeRequest(returningItems: [item], completionHandler: nil)
+//        } else {
+//            context.cancelRequest(withError: NSError(domain: "ContentBlocker", code: 0, userInfo: nil))
+//        }
+//    }
+//}
+
 import UIKit
 import MobileCoreServices
 
 class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
-        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        guard let url = Bundle.main.url(forResource: "blockerList", withExtension: "json"),
+              let attachment = NSItemProvider(contentsOf: url) else {
+            context.cancelRequest(withError: NSError(domain: "ContentBlocker", code: 1, userInfo: nil))
+            return
+        }
         
         let item = NSExtensionItem()
         item.attachments = [attachment]
-        
         context.completeRequest(returningItems: [item], completionHandler: nil)
     }
-    
 }
